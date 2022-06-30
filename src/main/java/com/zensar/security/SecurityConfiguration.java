37bda8d59b2c58fb63092575028dbb1fc91438fc
SM67965 <syed.m@zensar.com>
2022-06-30 17:57:11 +0530

    added authorization and jpa authentication

--- src/main/java/com/zensar/security/SecurityConfiguration.java
+++ src/main/java/com/zensar/security/SecurityConfiguration.java
@@ -1,31 +1,74 @@
 package com.zensar.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
+
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		@SuppressWarnings("deprecation")
-		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		UserDetails user = users
+			.username("user")
+			.password("user")
+			.roles("USER")
+			.build();
+		UserDetails admin = users
+			.username("admin")
+			.password("admin")
+			.roles("USER", "ADMIN")
+			.build();
+		
+		//JDBC logic
+		JdbcUserDetailsManager usersJdbc = new JdbcUserDetailsManager(dataSource());
+		usersJdbc.createUser(user);
+		usersJdbc.createUser(admin);
+		
+		System.out.println("========================>" + usersJdbc.getUsersByUsernameQuery());
+		
+		return usersJdbc;
 	}
 
 	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception { http.authorizeHttpRequests( (authz) -> authz.antMatchers("/**")
-	 * .hasRole("USER") .anyRequest() .authenticated()) .httpBasic() .and()
-	 * .formLogin(); return http.build(); }
+	 * @Bean public InMemoryUserDetailsManager userDetailsService() {
+	 * 
+	 * @SuppressWarnings("deprecation") UserDetails user =
+	 * User.withDefaultPasswordEncoder().username("user").password("user").roles(
+	 * "USER") .username("admin").password("admin").roles("ADMIN") .build(); return
+	 * new InMemoryUserDetailsManager(user);
+	 * 
+	 * }
 	 */
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz.antMatchers("/admin").hasRole("ADMIN").antMatchers("/user")
+				.hasAnyRole("USER","ADMIN").antMatchers("/").permitAll().anyRequest().authenticated()).httpBasic()
+				.and().formLogin();
+		return http.build();
+	}
+	
 
 }
--- src/main/java/test/HomeController.java
+++ src/main/java/test/HomeController.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +10,17 @@ import io.swagger.v3.oas.annotations.Hidden;
 @Hidden
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String sayHello() {
 		return "<h1> Welcome to Spring Boot</h1>";
 	}
 	
-	@RequestMapping("/admin")
+	@GetMapping("/admin")
 	public String getAdmin() {
 		return "<h1> Welcome to Admin dashboard</h1>";
 	}
 	
-	@RequestMapping("/user")
+	@GetMapping("/user")
 	public String getUser() {
 		return "<h1> Welcome to User dashboard</h1>";
 	}
