85592d506f79881ec0b0e2f3629b4a8e1c227bf2
SM67965 <syed.m@zensar.com>
2022-07-01 15:54:17 +0530

    Remove SOP

--- src/main/java/com/zensar/DemoApplication.java
+++ src/main/java/com/zensar/DemoApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.out.println("Hellllo!!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
