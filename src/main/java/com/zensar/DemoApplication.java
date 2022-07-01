1d08b7787c4db56927a832e4ece5296702c51fde
SM67965 <syed.m@zensar.com>
2022-07-01 15:46:21 +0530

    testing webhooks

--- src/main/java/com/zensar/DemoApplication.java
+++ src/main/java/com/zensar/DemoApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.out.println("Syed learnt Jenkins");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
