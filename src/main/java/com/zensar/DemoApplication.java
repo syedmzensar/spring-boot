69d7a02e671068659e565e99d7bb02f90e46e99b
SM67965 <syed.m@zensar.com>
2022-07-01 11:57:43 +0530

    added war file

--- src/main/java/com/zensar/DemoApplication.java
+++ src/main/java/com/zensar/DemoApplication.java
@@ -3,12 +3,14 @@ package com.zensar;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "test", "com.zensar" })
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
@@ -18,6 +20,12 @@ public class DemoApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
 	}
 
 }
